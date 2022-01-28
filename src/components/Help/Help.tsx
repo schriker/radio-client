1d74759e16f4a0768a473c5831374f0ac91121d7
Marcin Janus <janekmachine@gmail.com>
2022-01-28 16:21:08 +0100

    Add song label

--- src/components/Help/Help.tsx
+++ src/components/Help/Help.tsx
@@ -7,11 +7,16 @@ function Help() {
   return (
     <div className="ml-auto relative">
       <HelpModal isOpen={isOpen} setIsOpen={setIsOpen} />
-      <span className="animate-ping z-10 absolute inline-flex h-full w-full rounded-full bg-red-600"></span>
+      <span
+        onClick={() => setIsOpen(true)}
+        className="text-zinc-100 hidden sm:inline-block mr-2 hover:underline"
+      >
+        Dodaj utwór
+      </span>
       <button
         type="button"
         onClick={() => setIsOpen(true)}
-        className="relative z-20 rounded-full w-7 h-7 bg-zinc-500 text-zinc-100 font-semibold text-xs mr-0 hover:bg-zinc-700"
+        className="rounded-full w-7 h-7 bg-zinc-500 text-zinc-100 font-semibold text-xs mr-0 hover:bg-zinc-700"
       >
         ?
       </button>
