90459839daf9307c722fb4441ba23503ec251c21
Marcin Janus <janekmachine@gmail.com>
2022-01-28 15:55:24 +0100

    Help animation

--- src/components/Help/Help.tsx
+++ src/components/Help/Help.tsx
@@ -5,12 +5,13 @@ function Help() {
   let [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="ml-auto">
+    <div className="ml-auto relative">
       <HelpModal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <span className="animate-ping z-10 absolute inline-flex h-full w-full rounded-full bg-red-600"></span>
       <button
         type="button"
         onClick={() => setIsOpen(true)}
-        className="rounded-full w-6 h-6 bg-zinc-500 text-zinc-100 font-semibold text-xs mr-0 hover:bg-zinc-700"
+        className="relative z-20 rounded-full w-7 h-7 bg-zinc-500 text-zinc-100 font-semibold text-xs mr-0 hover:bg-zinc-700"
       >
         ?
       </button>
